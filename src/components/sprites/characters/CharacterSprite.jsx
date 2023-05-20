d7cf52b6a3ee64a80cd0ccf80a77dc694b94fba3
Arseid <yvesrobert.estrada@gmail.com>
2023-05-20 13:04:06 +0200

    inventory sprite display

--- src/components/sprites/characters/CharacterSprite.jsx
+++ src/components/sprites/characters/CharacterSprite.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SpriteImage from "../SpriteImage";
+import axios from "axios";
 
-const CharacterSprite = ({ name, face }) => {
-  return (
-    <SpriteImage
-      src={require(`../../../assets/characters/${name}/full/${face}.png`)}
-    />
-  );
+const CharacterSprite = ({ name, face = 'front' }) => {
+  const [sprite, setSprite] = useState(null);
+
+  useEffect(() => {
+    axios
+        .get("http://localhost:5000/yfus")
+        .then((response) => {
+          const data = response.data;
+          const selectedSprite = data.find((yfu) => yfu["prenom"] === name);
+          if (selectedSprite) {
+            let imageUrl;
+
+            if (face === "front") {
+              imageUrl = selectedSprite["full-front-url"];
+            } else if (face === "back") {
+              imageUrl = selectedSprite["full-back-url"];
+            } else if (face === "left") {
+              imageUrl = selectedSprite["full-left-url"];
+            } else if (face === "right") {
+              imageUrl = selectedSprite["full-right-url"];
+            }
+
+            setSprite(imageUrl);
+          }
+        })
+        .catch((error) => console.error(error));
+  }, [name, face]);
+
+  if (!sprite)
+      return <div>Loading...</div>
+
+  return <SpriteImage src={sprite} />;
 };
 
 export default CharacterSprite;
--- src/components/sprites/clothes/ClothingSprite.jsx
+++ src/components/sprites/clothes/ClothingSprite.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import SpriteImage from "../SpriteImage";
+import axios from "axios";
 
-const ClothingSprite = ({ category, name, face }) => {
+const ClothingSprite = ({ category, name, face = 'front' }) => {
+  const [sprite, setSprite] = useState(null);
+
+  useEffect(() => {
+    axios
+        .get("http://localhost:5000/clothes")
+        .then((response) => {
+          const data = response.data;
+          const selectedSprite = data.find((cloth) => cloth["name"] === name && cloth["type"] === category);
+          if (selectedSprite) {
+            let imageUrl;
+
+            if (face === "front") {
+              imageUrl = selectedSprite["front-url"];
+            } else if (face === "back") {
+              imageUrl = selectedSprite["back-url"];
+            } else if (face === "left") {
+              imageUrl = selectedSprite["left-url"];
+            } else if (face === "right") {
+              imageUrl = selectedSprite["right-url"];
+            }
+
+            setSprite(imageUrl);
+          }
+        })
+        .catch((error) => console.error(error));
+  }, [category, face, name]);
+
+  if (!sprite)
+    return <div>Loading...</div>
+
   return (
     <SpriteImage
-      src={require(`../../../assets/clothes/${category}/${name}/${face}.png`)}
+      src={sprite}
     />
   );
 };
