7c5d3c77b01a0b2e560e6b9603cc92b87d2ae4f5
Ryan Marin <ryan.marin8629@gmail.com>
2023-05-22 17:14:47 +0200

    tic tac toe design

--- src/pages/minigames/minigames.jsx
+++ src/pages/minigames/minigames.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useContext } from "react";
-import { Grid, Button } from "@mui/material";
+import { Grid, Button, Stack, Box } from "@mui/material";
 import UserDataContext from "../../context/UserDataContext";
 import axios from "axios";
 import UserContext from "../../context/UserContext";
+import { Close as CrossIcon, FavoriteBorder } from "@mui/icons-material";
 
 function Minigames() {
   const [board, setBoard] = useState(Array(9).fill(null));
-  const [player, setPlayer] = useState("X");
+  const [player, setPlayer] = useState("O");
   const [winner, setWinner] = useState(null);
   const user = useContext(UserContext);
   const { userData, setUserData } = useContext(UserDataContext);
@@ -21,13 +22,13 @@ function Minigames() {
     if (checkWinner(newBoard, player)) return;
 
     const emptyCells = newBoard
-        .map((value, pos) => [value, pos])
-        .filter(item => item[0] === null)
-        .map(item => item[1]);
+      .map((value, pos) => [value, pos])
+      .filter((item) => item[0] === null)
+      .map((item) => item[1]);
 
     const aiMove = emptyCells[Math.floor(Math.random() * emptyCells.length)];
 
-    newBoard[aiMove] = player === "X" ? "O" : "X";
+    newBoard[aiMove] = player === "O" ? "X" : "O";
     setBoard(newBoard);
 
     checkWinner(newBoard, newBoard[aiMove]);
@@ -50,16 +51,19 @@ function Minigames() {
       if (board[a] === player && board[b] === player && board[c] === player) {
         setWinner(player);
 
-        // If player 'X' wins, increment coins
-        if (player === 'X') {
+        // If player 'O' wins, increment coins
+        if (player === "O") {
           const newCoins = userData["coins"] + 1;
-          setUserData(prevUserData => ({ ...prevUserData, coins: newCoins }));
+          setUserData((prevUserData) => ({ ...prevUserData, coins: newCoins }));
 
           // Update coins in the backend
-          axios.put(`http://localhost:5000/users/${user["uid"]}`, { coins: newCoins })
-              .catch((error) => {
-                console.error("Failed to update user's data:", error);
-              });
+          axios
+            .put(`http://localhost:5000/users/${user["uid"]}`, {
+              coins: newCoins,
+            })
+            .catch((error) => {
+              console.error("Failed to update user's data:", error);
+            });
         }
 
         return true;
@@ -76,48 +80,70 @@ function Minigames() {
 
   const handleReset = () => {
     setBoard(Array(9).fill(null));
-    setPlayer("X");
+    setPlayer("O");
     setWinner(null);
   };
 
   return (
-    <div>
-      <Grid
-        container
-        spacing={1}
-        justifyContent="center"
-        sx={{ height: "500px", width: "500px" }}
-      >
-        {board.map((cell, index) => (
-          <Grid item key={index} xs={4}>
-            <Button
-              variant="contained"
-              size="large"
-              onClick={() => handleClick(index)}
-              disabled={cell || winner}
-              fullWidth
-              sx={{ height: "100%" }}
-            >
-              {cell}
-            </Button>
-          </Grid>
-        ))}
-      </Grid>
+    <Stack justifyContent="center" alignItems="center" sx={{ height: "100%" }}>
+      <Box>
+        <Grid
+          container
+          spacing={1}
+          justifyContent="center"
+          sx={{ width: "500px", height: "500px" }}
+        >
+          {board.map((cell, index) => (
+            <Grid item key={index} xs={4}>
+              <Button
+                variant="contained"
+                size="large"
+                onClick={() => handleClick(index)}
+                disabled={cell || winner}
+                fullWidth
+                sx={{
+                  position: "relative",
+                  bgcolor: index % 2 === 0 ? "#F7F" : "#FCE",
+                  borderRadius: "2rem",
+                  border: "0.25rem solid #FFF",
+                  height: "100%",
+                  boxSizing: "border-box",
+                  color: "white",
+                  boxShadow: "none",
+                  "&:hover": {
+                    bgcolor: index % 2 === 0 ? "#F7F" : "#FCE",
+                    border: "none",
+                  },
+                  "&:disabled": {
+                    bgcolor: index % 2 === 0 ? "#F7F" : "#FCE",
+                    border: "none",
+                    color: "white",
+                  },
+                }}
+              >
+                <Box sx={{ position: "absolute", fontSize: "3rem" }}>
+                  {cell && (cell === "X" ? <CrossIcon /> : <FavoriteBorder />)}
+                </Box>
+              </Button>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
       {winner && (
-          <div>
-            {winner === "draw" ? (
+        <Stack sx={{justifyContent:'center', alignItems:'center'}}>
+          {winner === "draw" ? (
                 <p>It's a draw!</p>
             ) : winner === "X" ? (
                 <p>You win! You gain a coin, you now have {userData["coins"]} coins! Good luck for your draws!</p>
             ) : (
                 <p>You lose! Better luck next time!</p>
             )}
-            <Button variant="contained" onClick={handleReset}>
-              Reset
-            </Button>
-          </div>
+          <Button variant="contained" onClick={handleReset}>
+            Reset
+          </Button>
+        </Stack>
       )}
-    </div>
+    </Stack>
   );
 }
 
